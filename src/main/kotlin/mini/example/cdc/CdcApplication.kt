f26b6d6b1f5e12925bebd07dc420782638a9c94e
hong9Lee <hongtemp9mac@gmail.com>
2024-09-14 18:43:46 +0900

    init

---
+++ src/main/kotlin/mini/example/cdc/CdcApplication.kt
@@ -0,0 +1,11 @@
+package mini.example.cdc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CdcApplication
+
+fun main(args: Array<String>) {
+	runApplication<CdcApplication>(*args)
+}
