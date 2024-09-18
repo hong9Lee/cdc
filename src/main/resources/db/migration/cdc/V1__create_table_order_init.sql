572778b74c5af24220a8ce4dd14c75639118ffd4
hong9Lee <hongtemp9mac@gmail.com>
2024-09-18 18:34:03 +0900

    feat: outbox 구현을 위한 테이블 생성 및 url 변경

--- src/main/resources/db/migration/cdc/V1__create_table_order_init.sql
+++ src/main/resources/db/migration/cdc/V1__create_table_order_init.sql
@@ -7,3 +7,14 @@ create table orders
     mod_date_time datetime(6) null comment '수정일자'
 ) comment '주문 테이블';
 
+
+create table orders_status_change_outbox
+(
+    seq           bigint auto_increment comment 'outbox 시퀀스' primary key,
+    order_id      varchar(64) not null comment '주문 ID',
+    outbox_id     varchar(64) not null comment 'outbox ID',
+    order_status  varchar(64) not null comment '주문 상태',
+    outbox_status varchar(64) not null comment 'outbox 상태 (init, send_success, send_fail)',
+    reg_date_time datetime(6) null comment '등록일자',
+    mod_date_time datetime(6) null comment '수정일자'
+)
