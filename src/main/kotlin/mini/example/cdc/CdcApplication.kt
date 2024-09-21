45d22620a6c453392044c22bc59d2608d3d8d1a7
hong9Lee <hongtemp9mac@gmail.com>
2024-09-21 15:55:44 +0900

    chore: CdcApplication async, redis 설정 추가

--- src/main/kotlin/mini/example/cdc/CdcApplication.kt
+++ src/main/kotlin/mini/example/cdc/CdcApplication.kt
@@ -1,9 +1,14 @@
 package mini.example.cdc
 
+import mini.example.cdc.config.RedisProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableConfigurationProperties(RedisProperties::class)
+@EnableAsync
 class CdcApplication
 
 fun main(args: Array<String>) {
